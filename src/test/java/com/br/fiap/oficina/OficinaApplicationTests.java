f5884bcf340dbacbcf4bfe005a36b9cc9361793e
Diego Pimenta <digopim@gmail.com>
2026-06-27 00:22:13 -0300

    initialize project structure with Spring Boot, PostgreSQL, and Maven wrapper

---
+++ src/test/java/com/br/fiap/oficina/OficinaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.br.fiap.oficina;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+//@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class OficinaApplicationTests {
+
+//	@Test
+//	@Disabled
+//	void contextLoads() {
+//	}
+
+}
